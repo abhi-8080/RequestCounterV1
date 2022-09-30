ee5084219b8cf3d8df9b9f9df8162dee570a0ef5
abhi-8080 <107861057+abhi-8080@users.noreply.github.com>
2022-09-30 15:55:32 +0200

    Added logger to print

--- src/main/kotlin/com/q/counter/SimpleCounter.kt
+++ src/main/kotlin/com/q/counter/SimpleCounter.kt
@@ -1,8 +1,15 @@
 package com.q.counter
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 class SimpleCounter : IAtomicCounter {
     var counter: Int = 0
 
     @Synchronized
-    override fun getNextVal(): Int = (++counter).also { println("value = $it") }
+    override fun getNextVal(): Int = (++counter).also { logger.info("value = $it") }
+
+    companion object{
+        val logger: Logger = LoggerFactory.getLogger(SimpleCounter::class.java)
+    }
 }
